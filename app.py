34fd18bcbaebfd6f72dcf318cac75dbc4199d8fa
theabdulsaboor <72669157+theabdulsaboor@users.noreply.github.com>
2025-01-05 22:50:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.sidebar.markdown("# TherapyAI")
 # Select AI model
 model_mapping = {
        "gpt2": "theabdulsaboor/gpt2-therapist-finetuned",
-       "distilgpt2": "Mrbean01/uzair_amiin", "gptneo": "Mrbean01/your_model_name", "test": "victunes/TherapyBeagle-11B-v1"  
+       "distilgpt2": "Mrbean01/uzair_amiin", "gptneo": "Mrbean01/your_model_name", "test": "victunes/TherapyBeagle-11B-v1"
    }
 selected_model_name = st.sidebar.selectbox("Change Model", list(model_mapping.keys()))  
 selected_model = model_mapping[selected_model_name]
